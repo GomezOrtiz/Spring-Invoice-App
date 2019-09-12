cec82b93059f23878c6d176bdeba5ccc6033ef4a
GomezOrtiz <david.gomez.mail@gmail.com>
2019-09-13 01:53:47 +0200

    After first succesful deploy to Heroku

--- src/main/java/com/fakecorp/invoicing/app/Application.java
+++ src/main/java/com/fakecorp/invoicing/app/Application.java
@@ -1,19 +1,13 @@
 package com.fakecorp.invoicing.app;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		//Se ejecuta al arrancar la aplicación
 	}
 
 }
